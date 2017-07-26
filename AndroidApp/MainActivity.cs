2b9834c24b20269bdda7c45277ce2fd76e2cc4ce
Juan Alejandro Rosales Coronel <jarc_software@hotmail.com>
2017-07-25 20:59:39 -0500

    Lab04 - Xamarin DIPLOMADO 3.0

---
+++ AndroidApp/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidApp
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            /* Creamos la instancia del código compartido
+            y le inyectamos la dependencia. */
+            var Validator = new PCLProyect.AppValidator(new AndroidDialog(this));
+            /* Aquí podríamos establecer los valores de las propiedades
+            * EMail, Password y Device*/
+            // Realizamos la validación
+            //Validator.Validate();
+
+            /* Aquí podríamos establecer los valores de las propiedades
+            * EMail, Password y Device*/
+            Validator.EMail = "jarc_software@hotmail.com";
+            Validator.Password = "Jrosales23";
+            Validator.Device =
+            Android.Provider.Settings.Secure.GetString(
+            ContentResolver,
+            Android.Provider.Settings.Secure.AndroidId);
+            // Realizamos la validación
+            Validator.Validate();
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ PCLProyect/AppValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCLProyect
+{
+    public class AppValidator
+    {
+        IDialog Dialog;
+        public AppValidator(IDialog platformDialog)
+        {
+            Dialog = platformDialog;
+        }
+
+        public string EMail { get; set; }
+        public string Password { get; set; }
+        public string Device { get; set; }
+        public async void Validate()
+        {
+            //string Result;
+            /* Aquí se puede implementar la funcionalidad
+            principal de la clase. Por el momento solo devuelve
+            una cadena fija. */
+            //Result = "¡Aplicación validada!";
+            /* Invocar el código específico de la plataforma */
+            //Dialog.Show(Result);
+
+            string Result;
+            var ServiceClient = new SALLab04.ServiceClient();
+            var SvcResult = await ServiceClient.ValidateAsync(
+                EMail, Password, Device);
+
+            Result =
+            $"{SvcResult.Status}\n{SvcResult.Fullname}\n{SvcResult.Token}";
+            
+            /* Invocar el código específico de la plataforma */
+            Dialog.Show(Result);
+        }
+    }
+}
